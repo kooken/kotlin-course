5c7a5822ec5e602aa7128d3e49ffe579958c2272
mariasazhina <msazhina23@gmail.com>
2024-09-13 16:56:01 +0300

    added hackathon hw

--- src/main/kotlin/com/kooken/kotlincourse/Main.kt
+++ src/main/kotlin/com/kooken/kotlincourse/Main.kt
@@ -1,4 +1,4 @@
-package com.kooken
+package com.kooken.com.kooken.kotlincourse
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
