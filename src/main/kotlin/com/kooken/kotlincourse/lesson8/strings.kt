4503c31fa94e2cc784eb6602f5b862afb0dc70da
mariasazhina <msazhina23@gmail.com>
2024-09-28 18:38:10 +0300

    small fixes

---
+++ src/main/kotlin/com/kooken/kotlincourse/lesson8/strings.kt
@@ -0,0 +1,19 @@
+package com.kooken.kotlincourse.lesson8
+
+fun main() {
+
+}
+
+// Rules of Verification and Transformation
+// If the phrase contains the word "impossible" - replace "impossible" with "definitely possible, just takes time."
+
+// If the phrase begins with "I'm not sure" - add ", but my gut tells me otherwise" to the end of the phrase
+
+
+// If the phrase contains the word "disaster" - replace "disaster" with "interesting event."
+
+
+// If the phrase ends with "no problem" - replace "no problem" with "with a few interesting challenges along the way."
+
+
+// If the phrase contains only one word - add "sometimes," before "and "but not always" after ".
